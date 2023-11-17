9c681fea24c180b580d7d233a6b1a233ff7a2768
Waterpig <wtryoutube@seznam.cz>
2023-11-17 14:04:47 +0100

    Oh hey

--- tgui/packages/tgui/interfaces/ZubbersSyndicateTrade.tsx
+++ tgui/packages/tgui/interfaces/ZubbersSyndicateTrade.tsx
@@ -6,7 +6,7 @@ import '../styles/themes/zubbers_syndicate.scss';
 
 export const ZubbersSyndicateTrade = (props, context) => {
   return (
-    <Window name={'Syndicate Trade Network'} width={600} height={600}>
+    <Window title={'Syndicate Trade Network'} width={600} height={600}>
       <Window.Content>
         <div class="theme_syndicate_menu">
           <Button>Test</Button>
