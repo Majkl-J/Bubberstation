f32e171ed88cdcb8d8b2ba48a041c9377f3a7422
Waterpig <wtryoutube@seznam.cz>
2023-11-19 13:05:06 +0100

    This all for like no fucking progress

--- tgui/packages/tgui/interfaces/ZubbersSyndicateTrade.tsx
+++ tgui/packages/tgui/interfaces/ZubbersSyndicateTrade.tsx
@@ -6,10 +6,19 @@ import '../styles/themes/zubbers_syndicate.scss';
 
 export const ZubbersSyndicateTrade = (props, context) => {
   return (
-    <Window title={'Syndicate Trade Network'} width={600} height={600}>
+    <Window
+      title={'Syndicate Trade Network'}
+      width={600}
+      height={600}
+      class="theme_syndicate_window">
       <Window.Content>
         <div class="theme_syndicate_menu">
-          <Button>Test</Button>
+          <div class="theme_syndicate_folder">
+            <div class="theme_syndicate_folder_buttons">
+              <Button class="theme_syndicate_button_folder">TEST MENU 1</Button>
+              <Button class="theme_syndicate_button_folder">TEST MENU 2</Button>
+            </div>
+          </div>
         </div>
       </Window.Content>
     </Window>
